3c8adc1381f6b7557b57215a54ace4cd16c48af4
cterrasid <clarette.terrasi@gmail.com>
2019-07-23 17:46:36 +0200

    Create container for look 2

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,6 +4,8 @@ import image from '../../assets/images.json';
 import Chasing from '../../images/textchasing.svg';
 import Spring from '../../images/textspring.svg';
 import Look01 from '../../images/look1-look1.st.jpg';
+import Look02 from '../../images/look2-look2.st.jpg';
+import imageLook02 from '../../images/chasingimg1.st.jpg';
 import './styles.scss';
 
 const Main = props => {
@@ -13,18 +15,43 @@ const Main = props => {
 
   return (
     <main className="main__container">
-      <div className={`logo-wrapper ${visibilityClass}`}>
-        <img className="logo__chasing" src={Chasing} alt={logo.alt}></img>
-        <img className="logo__spring" src={Spring} alt={sublogo.alt}></img>
-      </div>
-      <div className="look-one__wrapper">
-        <img className="look-one__image" src={Look01} alt="Look 01"></img>
-        <p className="look-one__description">
-          {`${looks.look_one}. `}
-          <strong>{`${data.serie}. `}</strong>
-          {`${data.name}.`}
-        </p>
-      </div>
+      <section className="main__wrapper">
+        <div className={`logo-wrapper ${visibilityClass}`}>
+          <img className="logo__chasing" src={Chasing} alt={logo.alt}></img>
+          <img className="logo__spring" src={Spring} alt={sublogo.alt}></img>
+        </div>
+        <div className="look-one__wrapper">
+          <img
+            className="look-one__image"
+            src={Look01}
+            alt={looks.look_one}
+          ></img>
+          <p className="look-one__description">
+            {`${looks.look_one}. `}
+            <strong>{`${data.serie}. `}</strong>
+            {`${data.name}.`}
+          </p>
+        </div>
+      </section>
+      <section className="look-two__container">
+        <div className="look-two__wrapper">
+          <img
+            className="look-two__image-one"
+            src={Look02}
+            alt={looks.look_two}
+          ></img>
+          <p className="look-one__description">
+            {`${looks.look_two}. `}
+            <strong>{`${data.serie}. `}</strong>
+            {`${data.name}.`}
+          </p>
+        </div>
+        <img
+          className="look-two__image-two"
+          src={imageLook02}
+          alt={looks.look_two}
+        ></img>
+      </section>
     </main>
   );
 };
