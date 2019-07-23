4e853b8f5b689e6d8cdd4b6065f8667ccba0fbd8
cterrasid <clarette.terrasi@gmail.com>
2019-07-23 17:56:03 +0200

    Update docs

--- docs/precache-manifest.591697cf6caaf1bb711cc89fd918d4e6.js
+++ docs/precache-manifest.591697cf6caaf1bb711cc89fd918d4e6.js
@@ -1,23 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "707b82d0b13824fb09ccd7b845d2fb33",
+    "revision": "c76a923e2b90cf6ad267fa584a522317",
     "url": "./index.html"
   },
   {
-    "revision": "83d5a1190bb8b6e9e326",
-    "url": "./static/css/main.a85d114a.chunk.css"
+    "revision": "29b95bb394f6950bae4d",
+    "url": "./static/css/main.dcb0d52a.chunk.css"
   },
   {
-    "revision": "740c377a24ddf128c7e6",
-    "url": "./static/js/2.41edc8e3.chunk.js"
+    "revision": "e1f42159f602730aa919",
+    "url": "./static/js/2.6af30b1e.chunk.js"
   },
   {
-    "revision": "83d5a1190bb8b6e9e326",
-    "url": "./static/js/main.71d7b0a6.chunk.js"
+    "revision": "29b95bb394f6950bae4d",
+    "url": "./static/js/main.723bb1c9.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
     "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "6c26471b73a5fce55c91099a3fed0f41",
+    "url": "./static/media/chasingimg1.st.6c26471b.jpg"
   },
   {
     "revision": "1385cd02fbd1b204d115098aaf581bb4",
@@ -26,6 +30,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "02959752635c8b2242caecb3480b2765",
     "url": "./static/media/look1-look1.st.02959752.jpg"
+  },
+  {
+    "revision": "ebc3d06d46923411f468489189223d14",
+    "url": "./static/media/look2-look2.st.ebc3d06d.jpg"
   },
   {
     "revision": "81182ac2bc630034037d1fdeea544c95",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.473c7a40b0c615b8136b94c35608ac56.js"
+  "./precache-manifest.591697cf6caaf1bb711cc89fd918d4e6.js"
 );
 
 self.addEventListener('message', (event) => {
